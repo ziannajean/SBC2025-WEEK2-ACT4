5e9ff0f35e437c6ca668d6a3d278a9dc46f04ad2
zianjean <herzianjean2@gmail.com>
2025-06-13 13:55:37 +0800

    Add login

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+app.route('/login')
+def about():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
  
